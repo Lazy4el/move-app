069577c0441191511e4e269fe7757a5c976cdc9c
Dmitriy Gavrilin <lxlxlxllist@gmail.com>
2022-06-28 19:06:47 +0300

    fix service

--- src/components/MovieServis/MovieServis.jsx
+++ src/components/MovieServis/MovieServis.jsx
@@ -3,7 +3,6 @@ import Cookies from "js-cookie";
 export default class MovieServis {
   _keyAPI = `1600e251544c3774f2b2cb7b8f3a251a`;
   _baseUrlAPI = `https://api.themoviedb.org/3/`;
-  _autorizteAPI = `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=`;
   _genreUrlAPI = `https://api.themoviedb.org/3/genre/movie/list?api_key=${this._keyAPI}&language=en-US`;
 
   _getResponse = async (url, option = {}) => {
@@ -12,7 +11,9 @@ export default class MovieServis {
 
   // Гостевая сессия
   getSession = async () => {
-    const response = await fetch(this._autorizteAPI + this._keyAPI);
+    const response = await this._getResponse(
+      `authentication/guest_session/new?api_key=${this._keyAPI}`
+    );
     if (!response.ok) throw new Error("Authorization failed");
     const session = await response.json();
     return session.guest_session_id;
@@ -21,10 +22,9 @@ export default class MovieServis {
   // Оцененные фильмы
   getMarkFilms = async (page = 1) => {
     const cookies = Cookies.get("guest_session_id");
-    const response = await fetch(
-      `https://api.themoviedb.org/3/guest_session/${cookies}/rated/movies?api_key=${this._keyAPI}&language=en-US&sort_by=created_at.asc&page=${page}`
+    const response = await this._getResponse(
+      `guest_session/${cookies}/rated/movies?api_key=${this._keyAPI}&language=en-US&sort_by=created_at.asc&page=${page}`
     );
-    if (!response.ok) throw new Error("Mark failed");
     const markFilmsList = await response.json();
     const markFilms = markFilmsList.results;
     const totalPages = markFilmsList.total_pages;
@@ -42,7 +42,7 @@ export default class MovieServis {
       },
       body: requestBody,
     };
-    this._getResponse(
+    await this._getResponse(
       `movie/${filmId}/rating?api_key=${this._keyAPI}&guest_session_id=${cookies}`,
       body
     );
